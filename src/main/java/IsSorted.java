2a1b147f362982a63e908c2ae5931b5bde48c254
vtran91 <vtran16@bellsouth.net>
2023-02-25 23:27:42 +0000

    Completed IsSorted Coding Challenge

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,10 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        for (int i = 0; i < arr.length -1; i++)
+        {
+            if (arr[i] >= arr[i+1]) return false;
+        }
+        return true;
     }
 }
